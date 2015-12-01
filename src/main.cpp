d4f3bf7dd00c65defe57535fe2d754c84e0ee59f
Jean-yves Yang <jean-yves.yang@mastertp>
2015-12-01 16:27:06 +0100

    ENH: first keyboard input for the character movement

--- src/main.cpp
+++ src/main.cpp
@@ -6,54 +6,101 @@ namespace ic = irr::core;
 namespace is = irr::scene;
 namespace iv = irr::video;
 
-struct MyEventReceiver : IEventReceiver
+class MyEventReceiver : public IEventReceiver
 {
-  bool OnEvent(const SEvent &event)
-  {
-    // If input is of keyboard type  (KEY_INPUT)
-    // and a pressed key
-    // and the key is ESCAPE
-    if (event.EventType == EET_KEY_INPUT_EVENT &&
-        event.KeyInput.PressedDown &&
-        event.KeyInput.Key == KEY_ESCAPE)
-      exit(0);
+public:
+    bool OnEvent(const SEvent &event)
+    {
+        // If input is of keyboard type  (KEY_INPUT)
+        // and a pressed key
+        // and the key is ESCAPE
+        if (event.EventType == EET_KEY_INPUT_EVENT &&
+                event.KeyInput.PressedDown &&
+                event.KeyInput.Key == KEY_ESCAPE)
+            exit(0);
+        if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+        return false;
+    }
 
-    return false;
-  }
+    virtual bool IsKeyDown(EKEY_CODE keyCode) const
+    {
+        return KeyIsDown[keyCode];
+    }
+    MyEventReceiver()
+    {
+        for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+            KeyIsDown[i] = false;
+    }
+private:
+    //store the state of each key
+    bool KeyIsDown[KEY_KEY_CODES_COUNT];
 };
 
 int main()
 {
-  // Event Receiver
-  MyEventReceiver receiver;
-  // Initialization of the rendering system and window
-  IrrlichtDevice *device = createDevice(iv::EDT_OPENGL,
-                                        ic::dimension2d<u32>(640, 480),
-                                        16, false, false, false, &receiver);
+    // Event Receiver
+    MyEventReceiver receiver;
+    // Initialization of the rendering system and window
+    IrrlichtDevice *device = createDevice(iv::EDT_OPENGL,
+                                          ic::dimension2d<u32>(640, 480),
+                                          16, false, false, false, &receiver);
 
-  iv::IVideoDriver  *driver = device->getVideoDriver();
-  is::ISceneManager *smgr = device->getSceneManager();
+    iv::IVideoDriver  *driver = device->getVideoDriver();
+    is::ISceneManager *smgr = device->getSceneManager();
 
-//  // Loading a mesh
-//  is::IAnimatedMesh *mesh = smgr->getMesh("data/tris.md2");
+    //  // Loading a mesh
+    is::IAnimatedMesh *mesh = smgr->getMesh("data/test_with_bones_and_skinweights_and_modif.x");
 
-//  // Creating node from mesh
-//  is::IAnimatedMeshSceneNode *node = smgr->addAnimatedMeshSceneNode(mesh);
+    //  // Creating node from mesh
+    is::IAnimatedMeshSceneNode *node = smgr->addAnimatedMeshSceneNode(mesh);
+    //ic::vector3df scale(0.005,0.005,0.005);
+    //node->setScale( scale );
 
+    is::ICameraSceneNode *camera = smgr->addCameraSceneNodeFPS();
+    //camera->setTarget(node->getPosition());
+    //is::ICameraSceneNode *camera = smgr->addCameraSceneNode(node, ic::vector3df(0,0,0),ic::vector3df(0,0,5),-1, true);
 
-  is::ICameraSceneNode *camera = smgr->addCameraSceneNodeMaya();
-//  camera->setTarget(node->getPosition());
+    // In order to do framerate independent movement, we have to know
+    // how long it was since the last frame
+    u32 then = device->getTimer()->getTime();
 
-  while(device->run())
-  {
-    driver->beginScene(true, true, iv::SColor(0,50,100,255));
+    // This is the movemen speed in units per second.
+    const f32 movementSpeed = 5.f;
 
-    // Draw the scene
-    smgr->drawAll();
-    //
-    driver->endScene();
-  }
-  device->drop();
+    while(device->run())
+    {
+        const u32 now = device->getTimer()->getTime();
+        const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
+        int lastFPS = -1;
+        then = now;
+        driver->beginScene(true, true, iv::SColor(0,50,100,255));
 
-  return 0;
+        core::vector3df nodePosition = node->getPosition();
+
+        if(receiver.IsKeyDown(irr::KEY_KEY_Q))
+            nodePosition.X -= movementSpeed * frameDeltaTime;
+        else if(receiver.IsKeyDown(irr::KEY_KEY_D))
+            nodePosition.X += movementSpeed * frameDeltaTime;
+
+        node->setPosition(nodePosition);
+
+        // Draw the scene
+        smgr->drawAll();
+        driver->endScene();
+        int fps = driver->getFPS();
+        if (lastFPS != fps)
+        {
+            core::stringw tmp(L"Movement Example - Irrlicht Engine [");
+            tmp += driver->getName();
+            tmp += L"] fps: ";
+            tmp += fps;
+
+            device->setWindowCaption(tmp.c_str());
+            lastFPS = fps;
+        }
+    }
+    device->drop();
+
+    return 0;
 }
